d2002b6e51b1c012c49c9a18d7c4c7319b3b8123
Pablo Moises Mazariegos Pinto <34450035+PabloMazariegos@users.noreply.github.com>
2019-09-19 00:09:37 -0600

    ajuste en lectura de huellas

--- src/Components/Mantenimiento/Mantenimiento.jsx
+++ src/Components/Mantenimiento/Mantenimiento.jsx
@@ -1,35 +1,40 @@
 import React from 'react';
 import Axios from 'axios';
 
+import Excel from '../Excel/Excel.jsx'
+
+
 
 class Mantenimiento extends React.Component {
 	constructor(props){
 		super(props);
 
 		this.state = {
-			empleados : []
+			empleados : [],
+			export : []
 		}
 
 		this.FindCodigo = this.FindCodigo.bind(this);
+		this.Ingresar = this.Ingresar.bind(this);
+		this.FormIngresar = this.FormIngresar.bind(this);
+		this.FindApellido = this.FindApellido.bind(this);
+		this.FindPuesto = this.FindPuesto.bind(this);
 	}
 
 	componentDidMount(){
 		Axios.get("http://localhost:60141/CICAM/Empleados/")
 			.then(response =>{
 				this.setState({
-					empleados : response.data
+					empleados : response.data,
+					alertType : ""
 				})
 			})
 	}
 
-	componentWillUnmount(){
-		Axios.Cancel();
-	}
-
 	FindCodigo(){
 		let IDEmp = document.getElementById("iCode").value
-		if(IDEmp != ""){
-			Axios.get("http://localhost:60141/CICAM/Empleados/Find?"+"ID="+ parseInt(IDEmp))
+		if(IDEmp !== ""){
+			Axios.get("http://localhost:60141/CICAM/Empleados/Find?ID="+ IDEmp)
 			.then(response => {
 				this.setState({
 					empleados : response.data
@@ -45,6 +50,93 @@ class Mantenimiento extends React.Component {
 		}
 	}
 
+	FindApellido(){
+		let IDEmp = document.getElementById("iApe").value
+		if(IDEmp !== ""){
+			Axios.get("http://localhost:60141/CICAM/Empleados/Find?APELLIDO="+ IDEmp)
+			.then(response => {
+				this.setState({
+					empleados : response.data
+				});
+			})
+		}else{
+			Axios.get("http://localhost:60141/CICAM/Empleados/")
+			.then(response =>{
+				this.setState({
+					empleados : response.data
+				})
+			})
+		}
+	}
+
+	FindPuesto(){
+		let IDEmp = document.getElementById("iPues").value
+		if(IDEmp !== ""){
+			Axios.get("http://localhost:60141/CICAM/Empleados/Find?PUESTO="+ IDEmp)
+			.then(response => {
+				this.setState({
+					empleados : response.data
+				});
+			})
+		}else{
+			Axios.get("http://localhost:60141/CICAM/Empleados/")
+			.then(response =>{
+				this.setState({
+					empleados : response.data
+				})
+			})
+		}
+	}
+
+	Ingresar(){
+		let Codigo = ""
+		let Nombre = document.getElementById("txt_nombre").value
+		let Apellido = document.getElementById("txt_apellido").value
+		let Puesto = document.getElementById("txt_puesto").value
+
+		if(Nombre !== ""	&& Apellido !== ""	&& Puesto !== ""){
+			Axios.post("http://localhost:60142/CICAM/Empleados?CODIGO="+ Codigo +"&NOMBRE="+Nombre+"&APELLIDO="+Apellido+"&PUESTO="+Puesto+"&HUELLA=''")
+			.then(response => {
+				if(response.data.StatusCode === 200){
+					alert(response.data.message);
+
+					Axios.get("http://localhost:60141/CICAM/Empleados/")
+					.then(response =>{
+						this.setState({
+							empleados : response.data
+						})
+					})
+
+				}
+			})
+		}
+	}
+
+	FormIngresar(){
+		document.getElementById('FormIngresar').style.display="block";
+	}
+
+	GrabaHuella(IDe){
+		document.getElementById(IDe+"spinner").style.display="block";
+		document.getElementById("AlertContainer").style.display="block";
+		document.getElementById("AlertMessage").innerHTML = "Coloca tu dedo en el lector 4 veces"
+		this.setState({alertType : "warning"});
+		
+		Axios.post("http://localhost:60142/CICAM/Marcaje?ID="+ IDe +"&NOMBRE=''&APELLIDO=''&PUESTO=''&HUELLA=''")
+			.then(response => {
+				if(response != ""){
+					document.getElementById(IDe+"spinner").style.display="none";
+					document.getElementById("AlertMessage").innerHTML = "Huella Grabada Exitosamente! "
+					this.setState({alertType : "success"});
+
+				}else{
+					this.setState({alertType : "danger"});
+					document.getElementById("AlertMessage").innerHTML = "Error Grabando Huella, intenta de nuevo!"
+					document.getElementById(IDe+"spinner").style.display="none";
+				}
+			})
+
+	}
   render() {
 		
 		return (
@@ -54,6 +146,14 @@ class Mantenimiento extends React.Component {
 					<h3 className="text-center">BUSQUEDA EMPLEADOS</h3>
 					</div>
 				</div>
+
+				<div className="row mt-5" id="AlertContainer" style={{display: "none"}}>
+          <div className="col-md-12">
+            <div className={`alert alert-${this.state.alertType}`} role="alert" id="AlertMessage">
+              
+            </div>
+          </div>
+        </div>
 
 				<div className="row justify-content-center mt-5" >
 					<div className="col-md-6 mt-md-0 mt-3 d-flex align-items-center justify-content-center flex-column"> 
@@ -65,9 +165,40 @@ class Mantenimiento extends React.Component {
 						</div>
 					</div>
 
+					<div className="input-group mb-3">
+						<input type="text" className="form-control" placeholder="Apellido" id="iApe"/>
+						<div className="input-group-append">
+							<button className="btn btn-outline-primary" type="button" id="btn-codigo" onClick={this.FindApellido}>Buscar</button>
+						</div>
+					</div>
+
+					<div className="input-group mb-3">
+						<input type="text" className="form-control" placeholder="Puesto" id="iPues"/>
+						<div className="input-group-append">
+							<button className="btn btn-outline-primary" type="button" id="btn-codigo" onClick={this.FindPuesto}>Buscar</button>
+						</div>
+					</div>
+
 						<div className="col-md-5 col-7 mt-3">          
-							<button className="btn btn-outline-secondary btn-block"  id="btn_Nuevo" name="btn_Nuevo">Nuevo</button>            
-							<button className="btn btn-outline-primary btn-block"  id="btn_Nuevo" name="btn_Nuevo">Reporte</button>            
+							<button className="btn btn-outline-secondary btn-block"  id="btn_Nuevo" name="btn_Nuevo" onClick={this.FormIngresar}>Nuevo</button>            
+							<Excel />           
+						</div>
+					</div>
+				</div>
+
+				<div id="FormIngresar" className="row mt-5" style={{display: 'none'}}>
+					<div className="offset-md-2 col-md-8">
+						<div className="form-group">
+							<input className="form-control" type="text" name="txt_nombre" id="txt_nombre" placeholder="Nombre"/>
+						</div>
+						<div className="form-group">
+							<input className="form-control" type="text" name="txt_apellido" id="txt_apellido" placeholder="Apellido"/>
+						</div>
+						<div className="form-group">
+							<input className="form-control" type="text" name="txt_puesto" id="txt_puesto" placeholder="Puesto"/>
+						</div>
+						<div className="form-group">
+							<button className="btn btn-success" onClick={this.Ingresar}>Ingresar</button>
 						</div>
 					</div>
 				</div>
@@ -81,7 +212,7 @@ class Mantenimiento extends React.Component {
 									<th scope="col">Nombres</th>
 									<th scope="col">Apellidos</th>
 									<th scope="col">Puesto</th>
-									<th scope="col">Huella</th>
+									<th colSpan="2" scope="col">Huella</th>
 								</tr>
 							</thead>
 							<tbody>					
@@ -96,9 +227,14 @@ class Mantenimiento extends React.Component {
 												<td>
 													{
 														this.state.empleados[index].HUELLA 
-															? <button className="btn btn-success" onClick={this.GrabaHuella}>Actualizar</button>
-															: <button className="btn btn-danger" onClick={this.GrabaHuella}>Grabar</button>
+															? <button className="btn btn-success" onClick={()=> this.GrabaHuella(this.state.empleados[index].ID)}>Actualizar</button>
+															: <button className="btn btn-danger" onClick={()=> this.GrabaHuella(this.state.empleados[index].ID)}>Grabar</button>
 													}
+												</td>
+												<td>
+												<div id={this.state.empleados[index].ID+"spinner"} style={{display: "none"}} className="spinner-border text-primary" role="status">
+													<span className="sr-only">Loading...</span>
+												</div>
 												</td>
 											</tr>
 										)
@@ -108,7 +244,8 @@ class Mantenimiento extends React.Component {
 						</table>
 					</div>
 				</div>
-			
+				
+				
 			</div>
 		)
 	}
